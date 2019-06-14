38c3bbf050b5a7c798483b0ba74d36964de32f14
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-14 11:42:36 -0700

    autho home

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from  'react';
+import CreatePlace from '../container/places/CreatePlace';
+// import AllPlaces from '../container/places/AllPlaces;
+
+export default function Home() {
+  return (
+    <>
+      <CreatePlace />
+      {/* <AllPlaces /> */}
+    </>
+  );
+}
