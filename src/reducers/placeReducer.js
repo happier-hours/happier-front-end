79aaf2a1a047f65386180db19a0f6c1ef7713b56
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-18 11:34:16 -0700

    added fetch

--- src/reducers/placeReducer.js
+++ src/reducers/placeReducer.js
@@ -1,6 +1,8 @@
 import {
   NEW_PLACE,
-  NEW_PLACE_PENDING
+  NEW_PLACE_PENDING,
+  FETCH_PLACES,
+  FETCH_PLACES_PENDING
 } from '../actions/placeAction';
 
 const initialState = {
@@ -14,6 +16,10 @@ export default function reducer(state = initialState, action) {
       return { ...state, loading: true };
     case NEW_PLACE:
       return { ...state, loading: false, list: [...state.list, action.payload] };
+    case FETCH_PLACES_PENDING:
+      return { ...state, loading: true };
+    case FETCH_PLACES:
+      return { ...state, loading: false, list: action.payload }
     default:
       return state;
   }
