2f7ed4323027ce5dfcefeaf7dfbba2c095830701
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-18 14:33:17 -0700

    get by id service written

--- src/services/happierApi.js
+++ src/services/happierApi.js
@@ -2,3 +2,4 @@ import { post, get } from './request';
 
 export const makePlace = place => post('/api/v1/places', place);
 export const getPlaces = () => get('/api/v1/places');
+export const getPlace = id => get(`/api/v1/places/${id}`);
