f3284c0218bb35838051ae71db3707791ad06a40
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-18 10:29:34 -0700

    created places component

---
+++ src/components/places/Places.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Place from './Place';
+
+function Places({ places }) {
+  const placeList = places.map(place => (
+    <li key={place._id}>
+      <Place place={place} />
+    </li>
+  ));
+  return (
+    <ul>
+      {placeList}
+    </ul>
+  );
+}
+
+Places.propTypes = {
+  places: PropTypes.array.isRequired
+};
