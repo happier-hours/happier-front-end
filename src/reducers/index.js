27a7f8e8bd8abfc04b0a5936bb2ec856dbba83db
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-18 14:33:03 -0700

    reducer index added detail

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import places from './placeReducer';
 import session from './sessionReducer';
+import detail from './placeDetailReducer';
 
 export default combineReducers({
   places,
-  session
+  session,
+  detail
 });
