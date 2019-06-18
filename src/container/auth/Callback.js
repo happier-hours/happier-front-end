ce661f84482e3b604a06116a480bca875422ea43
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-18 11:18:03 -0700

    added /add to push in callback

--- src/container/auth/Callback.js
+++ src/container/auth/Callback.js
@@ -21,7 +21,7 @@ const mapDispatchToProps = (dispatch, props) => ({
     const action = setSession();
     dispatch(action);
     action.payload.then(() => {
-      props.history.push('/');
+      props.history.push('/add');
     });
   }
 });
